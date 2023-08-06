34a35fb433f30fdd40329616f26d099296607a41
Andrew Butler <abutler911@gmail.com>
2023-08-05 20:34:17 -0600

    animation changes

--- main.js
+++ main.js
@@ -13,14 +13,25 @@ function randomChoice() {
   var display = document.getElementById("display");
 
   var intervalId = setInterval(function () {
-    var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
     display.textContent = choices[Math.floor(Math.random() * choices.length)];
   }, 10);
 
   setTimeout(function () {
     clearInterval(intervalId);
-    document.body.style.backgroundColor = "#f4f4f4";
     display.classList.add("choice-display-static");
+  }, 5000);
+
+  changeBackgroundColor();
+}
+
+function changeBackgroundColor() {
+  var intervalId = setInterval(function () {
+    var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+  }, 300);
+
+  setTimeout(function () {
+    clearInterval(intervalId);
+    document.body.style.backgroundColor = "#f4f4f4";
   }, 5000);
 }
